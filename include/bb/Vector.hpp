91d827b1c485e9e5d443f17d2b6b57d7599e8441
MortalFlame21 <markgio2100@.gmail.com>
2026-05-18 12:35:52 +1000

    style: Vector. Add doxygen comments to member functions.

--- include/bb/Vector.hpp
+++ include/bb/Vector.hpp
@@ -78,36 +78,66 @@ public:
     ~Vector() { delete[] beg_; }
 
     // element access
+    /// @brief Const reference access via operator[]().
+    /// @return `const_reference` of `i`th element in Vector container.
     const_reference operator[](size_type i) const { return beg_[i]; }
 
+    /// @brief Reference access via operator[]().
+    /// @return `reference` of `i`th element in Vector container.
     reference operator[](size_type i) { return beg_[i]; }
 
+    /// @brief Const reference access with size check.
+    /// @return `const_reference` of `i`th element in Vector container.
     const_reference at(size_type i) const {
         if (i >= size()) throw std::out_of_range{"Vector::at: i >= Vector::size"};
         return beg_[i];
     }
 
+    /// @brief Reference access with size check.
+    /// @return `reference` of `i`th element in Vector container.
     reference at(size_type i) {
         if (i >= size()) throw std::out_of_range{"Vector::at: i >= Vector::size"};
         return beg_[i];
     }
 
     // iterators
+    /// @brief Begin iterator access.
+    /// @return Begin `iterator`.
     iterator begin() const { return beg_; }
 
+    /// @brief Begin const iterator access.
+    /// @return Begin `const_iterator`.
     const_iterator begin() { return beg_; }
 
+    /// @brief End iterator access.
+    /// @return End `iterator`.
     iterator end() const { return end_; }
 
+    /// @brief End const_iterator access.
+    /// @return End `const_iterator`.
     const_iterator end() { return end_; }
 
     // size and capacity
+    /// @brief Returns how many elements Vector container can contain.
+    /// @return Return Vector container size.
     size_type size() const { return end_ - beg_; }
 
+    /// @brief Return how many elements Vector container has allocated.
+    /// @return Return Vector capacity size.
     size_type capacity() const { return cap_ - beg_; }
 
+    /// @brief Returns if Vector container is empty, i.e `begin() == end()`.
+    /// @return `bool` value if Vector is empty.
     bool empty() const { return begin() == end(); }
 
+    /// @brief Resize Vector size() to new_size.
+    /// @details
+    /// - if `size() > new_size`, decrease to new_size.
+    /// - if `size() == new_size`, do nothing.
+    /// - if `size() < new_size`, increase to new_size.
+    /// - if `capacity() < new_size`, `capacity()` grows to fit new_size.
+    /// @param new_size
+    /// @post `size() >= new_size`
     void resize(size_type new_size) {
         if (size() > new_size) {
             end_ -= size() - new_size;
@@ -120,6 +150,12 @@ public:
         }
     }
 
+    /// @brief Resize Vector `capacity()` to `new_capacity`.
+    /// @details
+    /// - If `capacity() >= new_capacity`, do nothing.
+    /// - If `capacity() < new_capacity`, `capacity()` grows to new_capacity.
+    /// @param new_capacity
+    /// @post `capacity() >= new_capacity`
     void reserve(size_type new_capacity) {
         if (capacity() < new_capacity) {
             T* container{new T[new_capacity]};
@@ -133,6 +169,9 @@ public:
     }
 
     // modifiers
+    /// @brief Pushes element `e` to back of Vector container.
+    /// @details if `capacity()` <= `size()`, `reserve()` is called to fit `e`.
+    /// @post `size()` is increased by one.
     void push_back(const T& e) {
         if (capacity() <= size())
             reserve(empty() ? 8 : 2 * size());
@@ -140,6 +179,9 @@ public:
         ++end_;
     }
 
+    /// @brief Pops the last element from the Vector container.
+    /// @details if `empty()` a `std::out_of_range` is thrown.
+    /// @post `size()` is decreased by one.
     void pop_back() {
         if (empty())
             throw std::out_of_range{"Vector::pop_back: Vector::empty == true"};
@@ -147,6 +189,9 @@ public:
         --end_;
     }
 
+    /// @brief clears the entire Vector container.
+    /// @post `begin()` is invalidated.
+    /// @post `end()` is invalidated.
     void clear() {
         delete[] beg_;
         beg_ = nullptr;
@@ -154,8 +199,13 @@ public:
         cap_ = nullptr;
     }
 
+    /// @brief Swaps current Vector with Vector `other`.
+    /// @param other
     void swap(Vector& other) { std::swap(*this, other); }
 
+    /// @brief Swaps Vector `a` with Vector `b`.
+    /// @param a
+    /// @param b
     friend void swap(Vector& a, Vector& b) { std::swap(a, b); }
 
 private:
